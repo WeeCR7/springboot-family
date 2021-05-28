eb2297a3c7a74da0978447ac41c881d04a653df2
zhanjiawei <1006099569@qq.com>
2021-05-28 16:25:46 +0800

    添加测试接口

--- src/main/java/com/wee/controller/TestController.java
+++ src/main/java/com/wee/controller/TestController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * @author zhanjiawei
  */
@@ -14,6 +18,15 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @GetMapping("/get")
+    public Map<String,Object> get(){
+        Map<String,Object> result = new HashMap<>();
+        result.put("code",1);
+        result.put("msg","succe");
+        result.put("data", Math.random());
+        return result;
     }
 
 }
