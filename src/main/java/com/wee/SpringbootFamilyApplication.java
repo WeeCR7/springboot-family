b95ae5c7e72ba7b5c830543d818416d9c94268c3
zhanjiawei <1006099569@qq.com>
2021-05-28 11:03:07 +0800

    项目初始化

---
+++ src/main/java/com/wee/SpringbootFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.wee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFamilyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wee/SpringbootFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFamilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
