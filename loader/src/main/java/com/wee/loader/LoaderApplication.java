c0ed92e556b377ed1608c24611f137eb53c2cec2
zhanjiawei <1006099569@qq.com>
2021-05-31 11:40:27 +0800

    添加启动模块

---
+++ loader/src/main/java/com/wee/loader/LoaderApplication.java
@@ -0,0 +1,16 @@
+package com.wee.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanjiawei
+ */
+@SpringBootApplication
+public class LoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaderApplication.class, args);
+    }
+
+}
